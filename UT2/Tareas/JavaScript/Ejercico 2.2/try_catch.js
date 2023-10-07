32514b4701b5c8f58024ee9ccd04471e84f3abb7
oscarlmj <oscarlmorenojorge@gmail.com>
2023-10-07 20:33:12 +0100

    subida

--- UT2/Tareas/JavaScript/Ejercico 2.2/try_catch.js
+++ UT2/Tareas/JavaScript/Ejercico 2.2/try_catch.js
@@ -1,4 +1,18 @@
-function mensaje(){
-    window.open('./try_catch2.html','_blank');
+function iniciar(){
     window.close();
+    window.open('./try_catch2.html','_blank');
+    
+    alert("Hola");
+}
+
+try{
+    const confirm=confirm("Haga click en aceptar para seguir viendo la pagina");
+
+    if(confirm===false){
+        throw new Error("El usuario hizo click en cancelar");
+    }
+}
+catch(error)
+{
+    console.error(error.message);
 }
--- UT2/Tareas/JavaScript/Ejercico 2.2/try_catch2.js
+++
@@ -1,7 +0,0 @@
-alert("Haz click en Continuar si quieres seguir viendo la página o click en Cancelar si quieres volver a la página inicial");
-
-function volver(){
-    window.open('./try_catch.html','_blank');
-    window.close();
-    
-}

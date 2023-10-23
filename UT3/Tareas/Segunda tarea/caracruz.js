b626ed708a756c586c13fa337b14a3c2bca6645a
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-10-23 10:44:42 +0100

    Control de errores

--- UT3/Tareas/Segunda tarea/caracruz.js
+++ UT3/Tareas/Segunda tarea/caracruz.js
@@ -1,42 +1,65 @@
 let moneda;
 
+/*
+Funcion principal, que llama a la funcion cambiarMoneda() en un interval para simular el lanzamiento de la moneda,
+y un timeout en el que se genera la cara la ganadora y se compara con la seleccionada mostrando el mensaje correspondiente.
+Se controla que el usuario haya seleccionado una cara, monstrando un mensaje de error en caso contrario.
+*/
 function lanzarMoneda() {
-    console.log(moneda);
-    let interval = setInterval(function(){
-        cambiarMoneda();
-    },150)
-
-    setTimeout(function(){
-        clearInterval(interval);
-        if(ganador()==moneda)
-        {
-            alert("Felicidade maquina")
-        }
-        else
-        alert("La rompefamilias");
-    },6000)
-
+    try{
+        if(moneda == null) throw new Error("Selecciona una moneda para poder jugar");
+        console.log("Has elegido "+moneda);
+        let interval = setInterval(function(){
+            cambiarMoneda();
+        },150)
+    
+        setTimeout(function(){
+            clearInterval(interval);
+            if(ganador()==moneda)
+            {
+                alert("Felicidade maquina")
+            }
+            else
+            {
+                if(moneda=="cara")
+                {
+                    alert("Que mala suerte, ha salido cruz");
+                }
+                else
+                {
+                    alert("Que mala suerte ha salido cara");
+                }
+            }
+        },6000)
+    }
+    catch(Error)
+    {
+        alert(Error);
+    }
 }
 
+//Funcion que aleatoriamente elige la cara ganadora.
 function ganador(){
     const random = Math.random();
 
     if (random < 0.5) {
         console.log("Salio cara");
+
         return "cara";
     } else {
         console.log("Salio cruz");
-
         return "cruz";
     }
 
 }
 
+//Funcion que recoge el valor de la cara seleccinada.
 function seleccionar(value){
     moneda=value;
-    console.log(value);
 }
 
+
+//Funcion que hace el cambio de la imagen de la moneda.
 function cambiarMoneda() {
     let img = document.getElementById("moneda");
 

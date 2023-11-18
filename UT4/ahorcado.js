61a09dc9e50cdac77a9e3eb1425d3ed5d387f9fe
oscarlmj <oscarlmorenojorge@gmail.com>
2023-11-18 12:36:42 +0000

    Ahorcado pero falta las expresiones regulares

--- UT4/ahorcado.js
+++ UT4/ahorcado.js
@@ -1,7 +1,9 @@
-const palabrasRAE = ["sol", "luz", "amor", "tiempo", "espacio", "vida", "color", "sueño", "alegría", "paz", "libertad", "historia", "cultura", "lengua", "sociedad", "trabajo", "familia", "arte"];
+const palabrasRAE = ["sol", "luz", "amor", "tiempo", "espacio", "vida", "color", "sueño", "alegria", "paz", "libertad", "historia", "cultura", "lengua", "sociedad", "trabajo", "familia", "arte"];
 
 let palabra;
-let caracteres
+let caracteres;
+let errores=[];
+let longitud;
 
 function darPalabra(){
     return palabrasRAE[Math.floor(Math.random() * palabrasRAE.length)];
@@ -9,42 +11,53 @@ function darPalabra(){
 
 function jugar(){
     palabra=darPalabra();
-    document.getElementById("jugador").disabled= false;
+    let jugada=document.getElementById("jugador");
+    jugada.disabled= false;
     caracteres=Array.from(palabra);
-
+    longitud=palabra.length;
     console.log(palabra);
     pista();
 }
 
 function pista()
 {
-    let espacios = [];  // Inicializa como un array vacío
-
+    let espacios = [];
+    
     let pista = caracteres[Math.floor(Math.random() * palabra.length)];
     
     for (let i = 0; i < palabra.length; i++) {
         if (palabra[i] === pista) {
             espacios.push('<td id="' + i + '" bgcolor="green">' + pista + '</td>');
+            longitud--;
         } else {
             espacios.push('<td id="' + i + '"></td>');
         }
     }
 
-    // Convierte el array en una cadena antes de asignarlo a innerHTML
     document.getElementById("jugar").innerHTML = espacios.join('');
 }
 
-function comprobar(){
+document.getElementById("jugador").addEventListener("keyup", function comprobar(){
     let caracter=document.getElementById("jugador").value;
-
     let pos=caracteres.indexOf(caracter);
-    if(pos!=-1)
+    console.log(longitud);
+    if(errores.length<5)
     {
-        document.getElementById(pos).innerHTML=caracter;
-        document.getElementById(pos).style.backgroundColor="green";
+        if(pos!=-1)
+        {
+            document.getElementById(pos).innerHTML=caracter;
+            document.getElementById(pos).style.backgroundColor="green";
+        }
+        else
+        {
+            errores.unshift('<td bgcolor="red">' + caracter + '</td>');
+            document.getElementById("errores").innerHTML=errores.join('');
+            console.log(errores);
+        }
+        document.getElementById("jugador").value="";
     }
-    else
+    else if(errores.length==5)
     {
-        document.getElementById("errores").innerHTML+='<td bgcolor="red">' + caracter + '</td>';
+        //Codigo de derrota.
     }
-}
+});

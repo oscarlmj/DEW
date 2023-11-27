f0f1fb3c315d9bdc323ecb73e21825819746b5af
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-11-27 12:26:09 +0000

    Tarjeta

--- UT5/tarjeta.js
+++ UT5/tarjeta.js
@@ -1,11 +1,3 @@
-const tarjetas = [
-    VISA= /^4[0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/,
-    MASTERCARD= /^5[1-5][0-9]{2}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/,
-    AMEX= /^3[47][0-9-]{16}$/,
-    CABAL= /^(6042|6043|6044|6045|6046|5896){4}[0-9]{12}$/,
-    NARANJA= /^(589562|402917|402918|527571|527572|0377798|0377799)[0-9]*$/
-];
-
 const objeto= {
     "visa": /^4[0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/,
     "mastercard": /^5[1-5][0-9]{2}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/,
@@ -14,20 +6,48 @@ const objeto= {
     "naranja": /^(589562|402917|402918|527571|527572|0377798|0377799)[0-9]*$/
 }
 
-
-
-function validarTarjeta(){
+function validar()
+{
     let usuario=document.getElementById("card").value;    
+    let cvv=document.getElementById("cvv").value;
+
+    try {
+        if(validarNumero())
+        {
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function validarNumero(){
+    let usuario=document.getElementById("card").value;    
+    let cvv=document.getElementById("cvv");
+
     try {
         if(usuario!== "")
         {
             for(let card in objeto)
             {   
                 let tipo=objeto[card];
+
                 if(tipo.test(usuario))
                 {
                     console.log("Esta tarjeta es una "+card);
                     document.getElementById(card).checked=true;
+
+                    if(card==="amex")
+                    {
+                        cvv.placeholder="XXXX";
+                        validarCVV(card);
+                    }
+                    else
+                    {
+                        document.getElementById("cvv").placeholder="XXX";
+                        validarCVV(card);
+                    }
                 }
             }
         }
@@ -35,7 +55,12 @@ function validarTarjeta(){
     } catch (error) {
         alert(error);
     }
+}
+
+function validarCVV(tarjeta)
+{
+    let longitud=objeto[tarjeta].length;
+    let cvv=document.getElementById("cvv");
+
     
-
-
 }

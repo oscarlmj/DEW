193f73317c245cc13e5544ae23d58c97966b505a
oscarlmj <oscarlmorenojorge@gmail.com>
2024-01-25 19:14:47 +0000

    Subida de cosas

---
+++ UT7/opener.js
@@ -0,0 +1,26 @@
+function abrir_ventana(){
+    let ventana = window.open("https://google.es", "Nueva ventana");
+}
+
+function abrir_caracteristicas() {
+    let opcionesVentana = 'width=420,height=430,menubar=no,status=yes,resizable=yes';
+    window.open('', '_blank', opcionesVentana);
+  }
+  
+function abrir_caracteristicas2(){
+    let posY = Math.floor(Math.random() * 501);
+  
+    let opcionesVentana = 'width=600,height=300,top=' + posY + ',left=150,menubar=no,status=no,scrollbars=no,directories=no,location=no,titlebar=no,toolbar=no';
+    window.open('url_de_tu_pagina.html', '_blank', opcionesVentana);
+}
+
+function completa(){
+    let screenWidth = window.screen.width;
+  let screenHeight = window.screen.height;
+
+  // Configurar opciones para la ventana a pantalla completa
+  let opcionesVentana = 'width=' + screenWidth + ',height=' + screenHeight + ',menubar=no,status=no,scrollbars=no,directories=no,location=no,titlebar=no,toolbar=no';
+
+  // Abrir la ventana con las opciones
+  window.open('', '_blank', opcionesVentana);
+}

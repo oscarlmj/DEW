f2d2d35feee1849b607a2422f79fd63b64aa4487
oscarlmj <oscarlmorenojorge@gmail.com>
2024-02-20 14:58:08 +0000

    Subida de cosas

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GithubService } from './services/github/github.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [GithubService]
+})
+export class AppComponent {
+  title = 'GitHub Searcher';
+}
---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

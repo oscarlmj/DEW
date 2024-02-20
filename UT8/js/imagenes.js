33ea51ecf03f4628ab0f7a6212d8d807e86d5432
oscarlmj <oscarlmorenojorge@gmail.com>
2024-02-20 23:46:57 +0000

    Cambio en la estructura de archivos

--- UT8/js/imagenes.js
+++ UT8/js/imagenes.js
@@ -1,25 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let primaria = document.getElementById('primaria');
-    let secundaria = document.getElementById('secundaria');
-    let secundariaInferior = document.getElementById('secundaria-inferior');
-let primariaInferior = document.getElementById('primaria-inferior');
+document.addEventListener("DOMContentLoaded", function () {
+  let primaria = document.getElementById("primaria");
+  let secundaria = document.getElementById("secundaria");
+  let secundariaInferior = document.getElementById("secundaria-inferior");
+  let primariaInferior = document.getElementById("primaria-inferior");
 
-    primaria.addEventListener('mouseover', function() {
-        primaria.src = './img/secun-dir.gif';
-        primariaInferior.src = './img/secun-indir.gif'
-        secundaria.src = './img/prim-dir.gif';
-        secundariaInferior.src= './img/prim-indir.gif'
-    });
+  primaria.addEventListener("mouseover", function () {
+    primaria.src = "../img/secun-dir.gif";
+    primariaInferior.src = "../img/secun-indir.gif";
+    secundaria.src = "../img/prim-dir.gif";
+    secundariaInferior.src = "../img/prim-indir.gif";
+  });
 
-    primaria.addEventListener('mouseout', function() {
-        primaria.src = './img/prim-dir.gif';
-        primariaInferior.src = './img/prim-indir.gif'
-        secundaria.src = './img/secun-dir.gif';
-        secundariaInferior.src= './img/secun-indir.gif'
+  primaria.addEventListener("mouseout", function () {
+    primaria.src = "../img/prim-dir.gif";
+    primariaInferior.src = "../img/prim-indir.gif";
+    secundaria.src = "../img/secun-dir.gif";
+    secundariaInferior.src = "../img/secun-indir.gif";
+  });
 
-    });
-
-    secundaria.addEventListener('click', function() {
-        window.close();
-    });
+  secundaria.addEventListener("click", function () {
+    window.close();
+  });
 });
